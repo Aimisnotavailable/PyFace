7c50e939c4c4828ac18beda9157d8b3fc4076372
Aimisnotavailable <aim.990111@gmail.com>
2024-12-28 22:50:14 +0800

    Added preliminary rotational values

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ import cv2
 import json
 import re
 import sys
+import math
+
+from projection import Cube
+
+X_ROT_IDX = (105, 334)
+Y_ROT_IDX = ((70, 107) , (300, 336))
+Z_ROT_IDX = (105, 334)
+
+def get_rot_z_angle(x, y):
+    return math.degrees(math.atan2(y, x)) * 0.6
+
+def get_rot_y_angle(len_difference):
+    return math.radians(len_difference + 90) * 0.6
+
+def get_dist(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+FACE_SCALE = 0
 
 screen = pygame.display.set_mode((600, 400))
 display = pygame.Surface((300, 200))
@@ -22,7 +40,10 @@ results = None
 
 points = None
 pygame.mouse.set_visible(False)
+
+
 while True:
+    
     render_points = []
     screen.fill((255, 255, 255))
     display.fill((255, 255, 255))
@@ -47,7 +68,7 @@ while True:
     mpos = pygame.mouse.get_pos() #[pygame.mouse.get_pos()[0] // 2, pygame.mouse.get_pos()[1] // 2]
     rect = pygame.Rect(*mpos, 2, 2)
     
-    
+    # print(get_rot_z_angle(render_points[386][0] - render_points[159][0], render_points[386][1] - render_points[159][1]))
     pygame.draw.rect(screen, (0, 0, 0), rect, 10)
     for idx, point in enumerate(render_points):
         
@@ -56,10 +77,18 @@ while True:
          
         pygame.draw.circle(screen, (0, 0, 0), point, 2)
     
+    FACE_SCALE = math.sqrt((render_points[159][0] - render_points[386][0]) ** 2 + (render_points[159][1] - render_points[386][1]) ** 2)
+    diff = get_dist(render_points[Y_ROT_IDX[0][0]], render_points[Y_ROT_IDX[0][1]]) - get_dist(render_points[Y_ROT_IDX[1][0]], render_points[Y_ROT_IDX[1][1]]) 
+    Cube().render(screen, angle_y=get_rot_y_angle(diff), angle_z=get_rot_z_angle(render_points[386][0] - render_points[159][0], render_points[386][1] - render_points[159][1]))
+    # print(height)
+    # print(math.sqrt((render_points[10][0] - render_points[152][0]) ** 2 + (render_points[10][1] - render_points[152][1]) ** 2) - height)
+    
     if render_points:
         for pairs in connections:
             pygame.draw.line(screen, (255, 0, 0), render_points[pairs[0]], render_points[pairs[1]])
-        
+    
+    pygame.draw.line(screen, (0, 0, 255), render_points[159], render_points[386])
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             cap.release()
--- projection.py
+++ projection.py
@@ -1,112 +1,86 @@
 import pygame
 from math import *
-
-WINDOW_SIZE =  800
-ROTATE_SPEED = 0.02
-window = pygame.display.set_mode( (WINDOW_SIZE, WINDOW_SIZE) )
-clock = pygame.time.Clock()
-
-projection_matrix = [[1,0,0],
-                     [0,1,0],
-                     [0,0,0]]
-
-cube_points = [n for n in range(8)]
-cube_points[0] = [[-1], [-1], [1]]
-cube_points[1] = [[1],[-1],[1]]
-cube_points[2] = [[1],[1],[1]]
-cube_points[3] = [[-1],[1],[1]]
-cube_points[4] = [[-1],[-1],[-1]]
-cube_points[5] = [[1],[-1],[-1]]
-cube_points[6] = [[1],[1],[-1]]
-cube_points[7] = [[-1],[1],[-1]]
+import numpy as np
 
 
-def multiply_m(a, b):
-    a_rows = len(a)
-    a_cols = len(a[0])
+class Cube:
+    
+    def __init__(self):
+        self.ROTATE_SPEED = 0.02
+        self.scale = 50
 
-    b_rows = len(b)
-    b_cols = len(b[0])
-    # Dot product matrix dimentions = a_rows x b_cols
-    product = [[0 for _ in range(b_cols)] for _ in range(a_rows)]
+        self.projection_matrix = [[1,0,0],
+                            [0,1,0],
+                            [0,0,0]]
 
-    if a_cols == b_rows:
-        for i in range(a_rows):
-            for j in range(b_cols):
-                for k in range(b_rows):
-                    product[i][j] += a[i][k] * b[k][j]
-    else:
-        print("INCOMPATIBLE MATRIX SIZES")
-    return product        
+        self.cube_points = [n for n in range(8)]
+        self.cube_points[0] = [[-1], [-1], [1]]
+        self.cube_points[1] = [[1],[-1],[1]]
+        self.cube_points[2] = [[1],[1],[1]]
+        self.cube_points[3] = [[-1],[1],[1]]
+        self.cube_points[4] = [[-1],[-1],[-1]]
+        self.cube_points[5] = [[1],[-1],[-1]]
+        self.cube_points[6] = [[1],[1],[-1]]
+        self.cube_points[7] = [[-1],[1],[-1]]
+        
+    def multiply_m(self, a, b) -> list:
+        return np.dot(a, b)       
 
 
-def connect_points(i, j, points):
-    pygame.draw.line(window, (255, 255, 255), (points[i][0], points[i][1]) , (points[j][0], points[j][1]))
+    def connect_points(self, surf, i, j, points) -> None:
+        pygame.draw.line(surf, (0, 0, 0), (points[i][0], points[i][1]) , (points[j][0], points[j][1]))
 
-# Main Loop
-scale = 100
-angle_x = angle_y = angle_z = 0
-while True:
-    clock.tick(60)
-    window.fill((0,0,0))
-    rotation_x = [[1, 0, 0],
+    def get_rotation_x(self, angle_x) -> list:
+        rotation_x = [[1, 0, 0],
                     [0, cos(angle_x), -sin(angle_x)],
                     [0, sin(angle_x), cos(angle_x)]]
+        return rotation_x
 
-    rotation_y = [[cos(angle_y), 0, sin(angle_y)],
-                    [0, 1, 0],
-                    [-sin(angle_y), 0, cos(angle_y)]]
+    def get_rotation_y(self, angle_y) -> list:
+        rotation_y = [[cos(angle_y), 0, sin(angle_y)],
+                        [0, 1, 0],
+                        [-sin(angle_y), 0, cos(angle_y)]]
+        return rotation_y
 
-    rotation_z = [[cos(angle_z), -sin(angle_z), 0],
-                    [sin(angle_z), cos(angle_z), 0],
-                    [0, 0, 1]]
-
-    points = [0 for _ in range(len(cube_points))]
-    i = 0
-    for point in cube_points:
-        rotate_x = multiply_m(rotation_x, point)
-        rotate_y = multiply_m(rotation_y, rotate_x)
-        rotate_z = multiply_m(rotation_z, rotate_y)
-        point_2d = multiply_m(projection_matrix, rotate_z)
+    def get_rotation_z(self, angle_z) -> list:
+        rotation_z = [[cos(angle_z), -sin(angle_z), 0],
+                        [sin(angle_z), cos(angle_z), 0],
+                        [0, 0, 1]]
+        return rotation_z
+    # Main Loop
     
-        x = (point_2d[0][0] * scale) + WINDOW_SIZE/2
-        y = (point_2d[1][0] * scale) + WINDOW_SIZE/2
 
-        points[i] = (x,y)
-        i += 1
-        pygame.draw.circle(window, (255, 0, 0), (x, y), 5)
-
-    connect_points(0, 1, points)
-    connect_points(0, 3, points)
-    connect_points(0, 4, points)
-    connect_points(1, 2, points)
-    connect_points(1, 5, points)
-    connect_points(2, 6, points)
-    connect_points(2, 3, points)
-    connect_points(3, 7, points)
-    connect_points(4, 5, points)
-    connect_points(4, 7, points)
-    connect_points(6, 5, points)
-    connect_points(6, 7, points)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    def render(self, surf, angle_x=0, angle_y=0, angle_z=0):    
+        points = [0 for _ in range(len(self.cube_points))]
+        i = 0
         
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_r]:
-            angle_y = angle_x = angle_z = 0
-        if keys[pygame.K_a]:
-            angle_y += ROTATE_SPEED
-        if keys[pygame.K_d]:
-            angle_y -= ROTATE_SPEED      
-        if keys[pygame.K_w]:
-            angle_x += ROTATE_SPEED
-        if keys[pygame.K_s]:
-            angle_x -= ROTATE_SPEED
-        if keys[pygame.K_q]:
-            angle_z -= ROTATE_SPEED
-        if keys[pygame.K_e]:
-            angle_z += ROTATE_SPEED      
-          
-    pygame.display.update()
+        
+        rotation_x = self.get_rotation_x(angle_x)
+        rotation_y = self.get_rotation_y(angle_y)
+        rotation_z = self.get_rotation_z(angle_z)
+        
+        for point in self.cube_points:
+            rotate_x = self.multiply_m(rotation_x, point)
+            rotate_y = self.multiply_m(rotation_y, rotate_x)
+            rotate_z = self.multiply_m(rotation_z, rotate_y)
+            point_2d = self.multiply_m(self.projection_matrix, rotate_z)
+        
+            x = (point_2d[0][0] * self.scale) + surf.get_width()//2
+            y = (point_2d[1][0] * self.scale) + surf.get_height()//2
+
+            points[i] = (x,y)
+            i += 1
+            pygame.draw.circle(surf, (255, 0, 0), (x, y), 5)
+
+        self.connect_points(surf, 0, 1, points)
+        self.connect_points(surf, 0, 3, points)
+        self.connect_points(surf, 0, 4, points)
+        self.connect_points(surf, 1, 2, points)
+        self.connect_points(surf, 1, 5, points)
+        self.connect_points(surf, 2, 6, points)
+        self.connect_points(surf, 2, 3, points)
+        self.connect_points(surf, 3, 7, points)
+        self.connect_points(surf, 4, 5, points)
+        self.connect_points(surf,4, 7, points)
+        self.connect_points(surf,6, 5, points)
+        self.connect_points(surf,6, 7, points)
